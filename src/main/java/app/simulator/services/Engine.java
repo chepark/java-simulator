c5d9d299048c10d19f44b7166fa6ef60bbbc9722
Chaeah Park <chaeah.park@taiste.com>
2023-12-10 18:09:32 +0200

    Simplifies the servicetype

--- src/main/java/app/simulator/services/Engine.java
+++ src/main/java/app/simulator/services/Engine.java
@@ -8,6 +8,8 @@ import app.simulator.models.*;
 import app.simulator.types.ServicePointType;
 import app.simulator.util.timeUtil.RandomTime;
 
+import static app.simulator.types.ServicePointType.*;
+
 public class Engine extends Thread {
     protected EventList eventList;
     private int numOfCustomers;
@@ -18,9 +20,9 @@ public class Engine extends Thread {
         eventList = new EventList<Event>();
         servicePoints = new ServicePoint[6];
 
-        servicePoints[0] = new ServicePoint(ServicePointType.QUEUE1, eventList); // Queue1
-        servicePoints[1] = new ServicePoint(ServicePointType.PANTTI, eventList); // Pantti
-        servicePoints[2] = new ServicePoint(ServicePointType.MARKET, eventList); // Market
+        servicePoints[0] = new ServicePoint(QUEUE1, eventList); // Queue1
+        servicePoints[1] = new ServicePoint(PANTTI, eventList); // Pantti
+        servicePoints[2] = new ServicePoint(MARKET, eventList); // Market
         servicePoints[3] = new ServicePoint(ServicePointType.QUEUE2, eventList); // Queue2
         servicePoints[4] = new ServicePoint(ServicePointType.SELF_CHECKOUT, eventList); // SelfCheckout
         servicePoints[5] = new ServicePoint(ServicePointType.CASHIER, eventList);  // Cashier
@@ -61,12 +63,13 @@ public class Engine extends Thread {
 
             System.out.println("Event" + eventToProcess.toString());
             t = eventToProcess.getTime();
-            switch (eventToProcess.getServicePoint()) {
-            case ServicePointType.ARRIVAL:
+            ServicePointType servicePointType = eventToProcess.getServicePoint();
+            switch (servicePointType) {
+            case ARRIVAL:
                 c = new Customer();
                 if (eventToProcess.hasPantti()) {
                     servicePoints[0].addToQueue(c);
-                    eventList.addEvent(new Event(t, ServicePointType.QUEUE1));
+                    eventList.addEvent(new Event(t, QUEUE1));
 
                     if (servicePoints[0].getHandledCustomers() == 0) {
                         servicePoints[0].setStartTime(t);
@@ -75,34 +78,34 @@ public class Engine extends Thread {
                     saveEventRecordToDB(servicePoints[0]);
                 } else {
                     servicePoints[2].addToQueue(c);
-                    eventList.addEvent(new Event(t, ServicePointType.MARKET));
+                    eventList.addEvent(new Event(t, MARKET));
                     // save event record to db
                     saveEventRecordToDB(servicePoints[2]);
                 }
                 break;
-            case ServicePointType.QUEUE1:
-                t = t + RandomTime.generate(ServicePointType.QUEUE1);
+            case QUEUE1:
+                t = t + RandomTime.generate(QUEUE1);
                 if (servicePoints[1].isIdle()) {
                     c = servicePoints[0].removeFromQueue();
                     servicePoints[0].setEndTime(t);
                     servicePoints[1].addToQueue(c);
-                    eventList.addEvent(new Event(t, ServicePointType.PANTTI));
+                    eventList.addEvent(new Event(t, PANTTI));
                     // save event record to db
                     saveEventRecordToDB(servicePoints[0]);
                     saveEventRecordToDB(servicePoints[1]);
                 }
                 break;
-            case ServicePointType.PANTTI:
+            case PANTTI:
                 c = servicePoints[1].removeFromQueue();
                 servicePoints[1].setIdle(true);
                 servicePoints[2].addToQueue(c);
                 t = t + RandomTime.generateShoppingTime();
-                eventList.addEvent(new Event(t, ServicePointType.MARKET));
+                eventList.addEvent(new Event(t, MARKET));
                 // save event record to db
                 saveEventRecordToDB(servicePoints[1]);
                 saveEventRecordToDB(servicePoints[2]);
                 break;
-            case ServicePointType.MARKET:
+            case MARKET:
                 if (servicePoints[3].isIdle() || servicePoints[2].getQueue().size() < 6) {
                     c = servicePoints[2].removeFromQueue();
 
@@ -111,21 +114,21 @@ public class Engine extends Thread {
                     }
 
                     servicePoints[3].addToQueue(c);
-                    t = t + RandomTime.generate(ServicePointType.QUEUE2);
-                    eventList.addEvent(new Event(t, ServicePointType.QUEUE2));
+                    t = t + RandomTime.generate(QUEUE2);
+                    eventList.addEvent(new Event(t, QUEUE2));
 
                     // save event record to db
                     saveEventRecordToDB(servicePoints[2]);
                     saveEventRecordToDB(servicePoints[3]);
                 }
                 break;
-            case ServicePointType.QUEUE2:
+            case QUEUE2:
                 if (servicePoints[4].isIdle()) {
                     c = servicePoints[3].removeFromQueue();
                     servicePoints[3].setEndTime(t);
                     servicePoints[4].addToQueue(c);
-                    t = t + RandomTime.generate(ServicePointType.SELF_CHECKOUT);
-                    eventList.addEvent(new Event(t, ServicePointType.SELF_CHECKOUT));
+                    t = t + RandomTime.generate(SELF_CHECKOUT);
+                    eventList.addEvent(new Event(t, SELF_CHECKOUT));
 
                     // save event record to db
                     saveEventRecordToDB(servicePoints[3]);
@@ -134,15 +137,15 @@ public class Engine extends Thread {
                     c = servicePoints[3].removeFromQueue();
                     servicePoints[3].setEndTime(t);
                     servicePoints[5].addToQueue(c);
-                    t = t + RandomTime.generate(ServicePointType.CASHIER);
-                    eventList.addEvent(new Event(t, ServicePointType.CASHIER));
+                    t = t + RandomTime.generate(CASHIER);
+                    eventList.addEvent(new Event(t, CASHIER));
 
                     // save event record to db
                     saveEventRecordToDB(servicePoints[3]);
                     saveEventRecordToDB(servicePoints[5]);
                 }
                 break;
-            case ServicePointType.SELF_CHECKOUT:
+            case SELF_CHECKOUT:
                 servicePoints[4].removeFromQueue();
                 servicePoints[3].setEndTime(t);
                 servicePoints[4].setIdle(true);
@@ -151,7 +154,7 @@ public class Engine extends Thread {
                 saveEventRecordToDB(servicePoints[4]);
 
                 break;
-            case ServicePointType.CASHIER:
+            case CASHIER:
                 servicePoints[5].removeFromQueue();
                 servicePoints[3].setEndTime(t);
                 servicePoints[5].setIdle(true);
