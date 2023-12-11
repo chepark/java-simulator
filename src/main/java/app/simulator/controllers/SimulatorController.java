241845f36e08e57d7bc594f7b1ea52dae0f2e07f
Chaeah Park <chaeah.park@taiste.com>
2023-12-11 21:02:09 +0200

    Fix speed control slider

--- src/main/java/app/simulator/controllers/SimulatorController.java
+++ src/main/java/app/simulator/controllers/SimulatorController.java
@@ -1,113 +1,116 @@
 package app.simulator.controllers;
 
 import app.simulator.dao.RecordDao;
-import app.simulator.entity.Record;
-import app.simulator.models.ServicePoint;
+import app.simulator.models.Record;
 import app.simulator.services.Engine;
 import app.simulator.util.timeUtil.RandomTime;
 import app.simulator.views.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 import java.util.List;
 
-
 public class SimulatorController {
-
-    @FXML
     public TextArea queue1_status;
-    @FXML
     public TextArea pantti_status;
-    @FXML
     public TextArea market_status;
-    @FXML
     public TextArea queue2_status;
-    @FXML
     public TextArea selfcheckout_status;
-    @FXML
-    public TextArea casher_status;
+    public TextArea cashier_status;
+    public Text simulator_status;
+    public Button btn_play;
+    public Button btn_stop;
     @FXML
     private TextField input_number;
     @FXML
     private Slider speed_slider;
-    @FXML
-    private Button btn_play;
-    @FXML
-    private Button btn_stop;
 
-    private App view;
     private Engine simulatorEngine;
     private List<Record> records;
     private RecordDao recordDao = new RecordDao();
+    private Writer writer;
+    private double speedRate = 1;
 
-    public void setAppReference(App view) {
-        this.view = view;
-    }
-
-    /***
-     * Display list of customer id waiting for returning pantti at Queue1.
-     */
-    public void setStatue(TextArea textArea, String string) {
+    public void setTextInput(TextArea textArea, String string) {
         textArea.clear();
         textArea.setText(string);
     }
 
+    public void setText(Text text, String string) {
+        text.setText(string);
+    }
 
-    /***
-     * Get the value and connect it to models-> Clock
-     */
     public void handleSliderRelease(MouseEvent e) {
-        // change the speed of clock on the model
-        double speed = speed_slider.getValue();
-        RandomTime.setSpeed(speed);
+        speedRate = speed_slider.getValue();
+        if (writer != null) {
+            writer.setSpeed(speedRate);
+        }
     }
 
-    /***
-     * Stop the simulation.
-     * @param e
-     */
-    public void handleStopClick(MouseEvent e) {
-        simulatorEngine.interrupt();
-        System.out.println("stop");
+    public void handleStopBtnClick(ActionEvent e) {
+        try {
+            if (writer != null) {
+                writer.interrupt();
+                setText(simulator_status, "Stopped");
+            }
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    // play button
-    public void playSimulation(ActionEvent e) throws InterruptedException {
-        // get customers from model and start to display in table
+    public void handlePlayBtnClick(ActionEvent e) throws InterruptedException {
         // reset Record table in DB
         recordDao.removeAllRecords();
+        setText(simulator_status, "Running");
 
+        // get input value from the UI
         int customers = Integer.parseInt(input_number.getText());
         if (customers == 0 || input_number.getText().equals("")) {
             System.out.println("Please enter the number of customers");
             return;
         }
+        // set the number of customers to generate arrival time
         RandomTime.setCustomerNumber(customers);
 
+        // simulator main logic engine starts
+        // simulator logic result will be saved in DB
         simulatorEngine = new Engine();
         simulatorEngine.start();
         simulatorEngine.join();
 
+        // once engine is done, get all accumulated records from DB and start writer
         records = recordDao.getAllRecords();
-        Writer writer = new Writer(records);
-        Thread thread = new Thread(writer);
-        thread.start();
+        writer = new Writer(records);
+        writer.start();
     }
-
-    public class Writer implements Runnable {
+    
+    // Write the records to the UI
+    public class Writer extends Thread {
         private List<Record> records;
-        private TextArea textArea;
-        private String string;
-
+        // set up the speed to volatile to make sure the speed can be updated while the thread is running.
+        // default speed to display the customers on the UI: 1000ms = 1s
+        private double speed = 1000;
         public Writer(List<Record> records) {
             this.records = records;
-            this.textArea = textArea;
-            this.string = string;
+        }
+
+        public void setSpeed(double speedRate) {
+            if (speedRate == 0) {
+                speed = 4000;
+            } else if (speedRate == 0.5) {
+                speed = 3000;
+            } else if (speedRate == 1.0) {
+                speed = 2000;
+            } else if (speedRate == 1.5) {
+                speed = 1000;
+            } else if (speedRate == 2.0) {
+                speed = 500;
+            }
         }
 
         @Override
@@ -115,34 +118,28 @@ public class SimulatorController {
             for (Record r : records) {
                 String servicePoint = r.getServicePoint();
                 switch (servicePoint) {
-                case "Queue1":
-                    setStatue(queue1_status, r.getCustomers());
+                case "QUEUE1":
+                    setTextInput(queue1_status, r.getCustomers());
                     break;
-
-                case "Pantti":
-                    setStatue(pantti_status, r.getCustomers());
+                case "PANTTI":
+                    setTextInput(pantti_status, r.getCustomers());
                     break;
-
                 case "MARKET":
-                    setStatue(market_status, r.getCustomers());
+                    setTextInput(market_status, r.getCustomers());
                     break;
-
                 case "QUEUE2":
-                    setStatue(queue2_status, r.getCustomers());
+                    setTextInput(queue2_status, r.getCustomers());
                     break;
-
                 case "SELF_CHECKOUT":
-                    setStatue(selfcheckout_status, r.getCustomers());
+                    setTextInput(selfcheckout_status, r.getCustomers());
                     break;
-
                 case "CASHIER":
-                    setStatue(casher_status, r.getCustomers());
+                    setTextInput(cashier_status, r.getCustomers());
                     break;
-
                 }
-
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep((long) speed);
+                    System.out.println("Speed: " + speed);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
